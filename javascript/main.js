7fde4f5e62150c31ff9b22f546b81445839b404b
Jorge Berrizbeitia <58472035+jorgeberrizbeitia@users.noreply.github.com>
2022-02-03 18:46:56 +0100

    lecture done

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,36 @@
 // * GLOBAL VARIABLES
 
-
-
+let splashScreen = document.querySelector("#splash-screen")
+let gameOverScreen = document.querySelector("#gameover-screen")
+let canvas = document.querySelector("#my-canvas")
+let ctx = canvas.getContext("2d")
+// let newGame = new Game()
+let newGame;
 
 
 // * STATE MANAGEMENT FUNCTIONS
 
+const startGame = () => {
 
+  // desaparer el splash screen y aparecer canvas
+  splashScreen.style.display = "none";
+  canvas.style.display = "flex";
 
+  // ejecutar mi juego
+  newGame = new Game()
+  console.log(newGame)
+  newGame.gameLoop()
+
+}
 
 
 
 // * ADD EVENT LISTENERS
 
+let startButton = document.querySelector("#start-btn")
+startButton.addEventListener("click", startGame)
 
-
-
-
+canvas.addEventListener("click", () => {
+  // como hago para invocar la funcion polloJump ???
+  newGame.pollo.polloJump()
+})
